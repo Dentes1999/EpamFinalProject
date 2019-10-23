6e56c04aab376b70f38a5259abfbde80f3a1b510
den_kenobi <dteslenko777@gmail.com>
2019-10-23 18:02:22 +0300

    Добавьте файлы проекта.

---
+++ EpamFinalProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EpamFinalProject.Startup))]
+namespace EpamFinalProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
